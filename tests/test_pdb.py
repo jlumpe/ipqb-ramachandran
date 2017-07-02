cd75ac3990625432d9ed83e13af7bdf086d60d01
Jared Lumpe <mjlumpe@gmail.com>
2017-07-02 14:53:07 -0700

    Tests for PDB parsing

---
+++ tests/test_pdb.py
@@ -0,0 +1,44 @@
+"""Test PDB file parsing."""
+
+import pytest
+import numpy as np
+from Bio import PDB
+
+from ramachandran import parse_pdb_chain
+
+
+@pytest.fixture
+def pdbfile():
+	"""Path to a PDB file to parse. You'll need to actually put it there."""
+	return 'structures/12asA00'
+
+
+def test_parse_chain(pdbfile):
+	"""Test parsing residues from a PDB file vs the BioPython implementation."""
+
+	# Parse using our code
+	with open(pdbfile) as fobj:
+		residues1 = list(parse_pdb_chain(fobj))
+
+	# Parse using BioPython
+	parser = PDB.PDBParser()
+	structure = parser.get_structure('test', pdbfile)
+	residues2 = list(structure.get_residues())
+
+	assert len(residues1) == len(residues2)
+
+	# Compare residues
+	for res1, res2 in zip(residues1, residues2):
+
+		# Residue attributes
+		assert res1.name == res2.resname
+		assert res1.seq == res2.id[1]
+
+		# Compare atoms
+		assert len(res1.atoms) == len(res2)
+
+		# Both should be in the same order they were in in the file...
+		for a1, a2 in zip(res1.atoms, res2):
+			assert a1.name == a2.name
+			assert np.allclose(a1.coord, a2.coord)
+			assert a1.serial == a2.serial_number
